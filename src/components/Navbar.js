e78ec542afc9dc660e63f7ac204e50f3ccbcb5d3
Evelyn <evelynveronica1412@gmail.com>
2024-11-23 00:04:15 +1300

    navigation bar base done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,38 +5,38 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className='navbar'>
-    <div className='navbar-container'>
-        {/* Logo */}
-        <Link to="/" className='navbar-logo'>
-            <img src='/logo.png' alt='Logo'/>
-        </Link>
+        <div className='navbar-container'>
+            {/* Logo */}
+            <Link to="/" className='navbar-logo'>
+                <img src='/logo.png' alt='Logo'/>
+            </Link>
 
-        {/* Navigation Links */}
-        <ul className='navbar-links'>
-            <li>
-                <a href='/about'>About</a>
-            </li>
-            <li>
-                <a href='/projects'>Projects</a>
-            </li>
-            <li>
-                <a href='/skills'>Skills</a>
-            </li>
-            <li>
-                <a href='/contact'>Contact</a>
-            </li>
-        </ul>
+            {/* Navigation Links */}
+            <ul className='navbar-links'>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/projects'>Projects</Link>
+                </li>
+                <li>
+                    <Link to='/skills'>Skills</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+            </ul>
 
-        {/* Social media icons */}
-        <div className='navbar-icons'>
-            <a href='https://www.linkedin.com/in/evelyn-salim' target='_blank' rel='noopener noreferrer'>
-                <i className='fab fa-linkedin'></i>
-            </a>
-            <a href='https://github.com/evelynvrsx' target='_blank' rel='noopener noreferrer'>
-                <i className='fab fa-github'></i>
-            </a>
+            {/* Social media icons */}
+            <div className='navbar-icons'>
+                <a href='https://www.linkedin.com/in/evelyn-salim' target='_blank' rel='noopener noreferrer'>
+                    <i className='fab fa-linkedin'></i>
+                </a>
+                <a href='https://github.com/evelynvrsx' target='_blank' rel='noopener noreferrer'>
+                    <i className='fab fa-github'></i>
+                </a>
+            </div>
         </div>
-    </div>
     </nav>
   )
 }
