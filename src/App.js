c2d6ade581ddc3146e7529fe9d12ded04c9eb72b
Evelyn <evelynveronica1412@gmail.com>
2024-11-23 20:31:40 +1300

    about page description & scroll circle updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+
+// import pages
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import Skills from './components/pages/Skills';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <Navbar />
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
 }
 
