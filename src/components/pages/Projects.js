e34862b9970c272f69407e3390d1f786de8b32f1
Evelyn <evelynveronica1412@gmail.com>
2025-02-22 18:51:08 +1300

    Projects section done

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -49,16 +49,17 @@ export default function Projects() {
       title: "Chips Challenge",
       image: "./chips_challenge.png",
       description: "Developed a Chip’s Challenge-inspired game in a team of 6, focusing on game mechanics, level design, and GUI rendering using Java and its design patterns.\n\n# Java | # GUI | # Object Oriented Programming",
-      fullDescription: "This is the full description for Chips Challenge. It includes more details about the project, technologies used, and challenges faced.",
+      fullDescription: "Remade the game Chip's Challenge with a Batman theme as part of a team of six, utilizing Java and Object-Oriented Programming (OOP). The project followed the Model-View-Controller (MVC) design pattern, along with additional design patterns applied to various classes to ensure a well-structured and maintainable codebase. Version control and collaboration were managed using GitLab. Responsible for graphical rendering and user experience, the renderer role involved displaying game elements, drawing sprites, managing the GUI, and integrating player interactions. Additionally, implemented JSON file handling to reload saved replays, ensuring smooth game progression. Audio effects and background music were incorporated to enhance the overall experience. With each team member assigned distinct roles, GitLab facilitated efficient coordination throughout development.",
       additionalImages: ["./chips_challenge_1.png", "./chips_challenge_2.png"],
       videoLink: "./chips_challenge_video.mp4",
+      youtubeLink: "https://youtu.be/lwYQM3DuwEM?si=TjV8AeDddJr1MJ3c", 
     },
     {
       id: 2,
       title: "3D Adventure Time Game",
       image: "./bmo_game.png",
       description: "Created an Adventure Time 3D game using Unreal Engine 5 and Blender\n\n# Unreal Engine 5 | # Blender | # Game Development",
-      fullDescription: "This is the full description for the 3D Adventure Time Game. It includes more details about the project, technologies used, and challenges faced.",
+      fullDescription: "I developed a 3D Adventure Time-inspired game using Unreal Engine 5 and Blueprints, designing and animating all assets in Blender. The game follows BMO, the main character, as they navigate obstacles to reach the house, which serves as the finish line. I incorporated physics-based mechanics, including springs for higher jumps and smashable wooden crates, to enhance gameplay. Version control was managed using GitHub, ensuring efficient collaboration and development.",
       additionalImages: ["./bmo_game_1.png", "./bmo_game_2.png"],
       videoLink: "./bmo_game_video.mp4",
     },
@@ -67,8 +68,9 @@ export default function Projects() {
       title: "Spill the Tea",
       image: "./spill_the_tea.png",
       description: "Developed a bubble tea website and implemented MySQL database integration to manage product stocks via a login system\n\n#HTML | #CSS | #MySQL",
-      fullDescription: "This is the full description for Spill the Tea. It includes more details about the project, technologies used, and challenges faced.",
+      fullDescription: "Developed a fully responsive bubble tea e-commerce website with a customer-facing storefront and a staff management system. The website was built using PHP, MySQL, HTML, CSS, and JavaScript, with a secure login system for staff that featured encrypted passwords. Staff members could add, update, and remove products, as well as view customer orders directly through the system. A MySQL database was implemented to store product details, customer orders, and staff credentials, ensuring efficient data management. The entire website was designed and coded from scratch, using free images from Shutterstock for visuals. Throughout development, Trello was utilized for project management, and continuous feedback was gathered to enhance the site's functionality and user experience.",
       additionalImages: ["./spill_the_tea_1.png", "./spill_the_tea_2.png"],
+      externalLink: "https://trello.com/invite/b/62cb59e728b6c349ee3964ff/ATTIca092edae237105b6066f1aa7dace194FA1009C4/spill-the-tea",
     },
     {
       id: 4,
@@ -83,7 +85,7 @@ export default function Projects() {
       title: "Autonomous Vehicle Challenge (AVC)",
       image: "./avc.png",
       description: "Designed and executed an autonomous robot in a team of 4 to navigate a course using pixel information\n\n #C++ #Raspberry Pi #GitLab",
-      fullDescription: "The Autonomous Vehicle Challenge (AVC) was a robotics and programming project in which I collaborated with a team of four to design, build, and programme an autonomous robot capable of navigating a complicated course. We programmed the robot in C++, using a Raspberry Pi as the primary controller, and used PID (Proportional Integral Derivative) control to follow lines, avoid obstacles, and interact with its surroundings. The project involved the integration of hardware components such as servos, a PCB, and a camera, as well as software techniques such as computer vision for detecting coloured objects and WiFi connectivity for gate opening. Despite time limits, we finished 80% of the course, demonstrating strong problem-solving, teamwork, and technical skills. GitLab is used to manage version control throughout the process.",
+      fullDescription: "The Autonomous Vehicle Challenge (AVC) was a robotics and programming project in which I collaborated with a team of four to design, build, and programme an autonomous robot capable of navigating a complicated course. We programmed the robot in C++, using a Raspberry Pi as the primary controller, and used PID (Proportional Integral Derivative) control to follow lines, avoid obstacles, and interact with its surroundings. The project involved the integration of hardware components such as servos, a PCB, and a camera, as well as software techniques such as computer vision for detecting coloured objects and WiFi connectivity for gate opening. Despite time limits, we finished 80% of the course, demonstrating strong problem-solving, teamwork, and technical skills. GitLab is used to manage version control throughout the process.",
       additionalImages: ["./avc_1.png", "./avc_2.jpg"],
       reportLink: "./avc_report.pdf",
     },
@@ -106,7 +108,6 @@ export default function Projects() {
             <img src={project.image} alt={project.title} className='w-100' />
             <div className='card-body'>
               <h3>{project.title}</h3>
-              {/* Updated to correctly display new lines */}
               <p>
                 {project.description.split('\n').map((line, index) => (
                   <React.Fragment key={index}>
@@ -137,6 +138,28 @@ export default function Projects() {
                 <source src={selectedProject.videoLink} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
+            )}
+            {/* Add a button to link to the YouTube video */}
+            {selectedProject.youtubeLink && (
+              <a
+                href={selectedProject.youtubeLink}
+                target="_blank" // Open the link in a new tab
+                rel="noopener noreferrer" // Recommended for security
+                className="youtube-button"
+              >
+                Watch on YouTube
+              </a>
+            )}
+            {/* Add a button for the external link (e.g., Trello) */}
+            {selectedProject.externalLink && (
+              <a
+                href={selectedProject.externalLink}
+                target="_blank" // Open the link in a new tab
+                rel="noopener noreferrer" // Recommended for security
+                className="external-link-button"
+              >
+                View on Trello
+              </a>
             )}
             {/* Add a download button for the report (if applicable) */}
             {selectedProject.reportLink && (
